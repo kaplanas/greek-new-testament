0ecaae3c307d3954f321262b42caef2e0f13a2ed
Abby Kaplan <kaplanas@gmail.com>
2021-09-18 10:56:16 -0600

    allow IP verb complements

--- text_and_vocab/process_vocab.py
+++ text_and_vocab/process_vocab.py
@@ -11,7 +11,8 @@ HAND_DATA_DIR = 'hand_coded_data/'
 SUB_FEAT_STRUCTS = {
     'AGR': ['PERSON', 'NUMBER', 'GENDER'],
     'ARGS': ['ARG_NONE', 'ARG_ACC', 'ARG_DAT', 'ARG_GEN', 'ARG_INF', 'ARG_OTI',
-             'ARG_ACC_ACC', 'ARG_ACC_DAT', 'ARG_DAT_GEN', 'ARG_DAT_INF']
+             'ARG_IP', 'ARG_ACC_ACC', 'ARG_ACC_DAT', 'ARG_DAT_GEN',
+             'ARG_DAT_INF']
 }
 POS_PROCESSING = {
     'noun': {'id_cols': ['gender'],
