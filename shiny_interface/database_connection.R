2dd4ae75e47a1538e17f9ece49a2ff435209f3b7
abby <kaplanas@gmail.com>
2024-12-12 23:10:28 -0700

    initial skeleton of Shiny app

---
+++ shiny_interface/database_connection.R
@@ -0,0 +1,4 @@
+db.user = "colemil_wsf"
+db.password = "kaplanas"
+db.name = "colemil_wsf"
+db.host = "colemillroad.org"
---
+++ shiny_interface/database_connection_local.R
@@ -0,0 +1,3 @@
+db.user = "shiny"
+db.password = "Gr33kT0M3"
+db.host = "localhost"
